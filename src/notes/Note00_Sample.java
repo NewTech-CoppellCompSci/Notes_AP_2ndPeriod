51a69a2534d84e54f161dc699e141724c102e556
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2021-08-26 14:20:29 -0500

    Try Catch Example

--- src/notes/Note00_Sample.java
+++ src/notes/Note00_Sample.java
@@ -1,10 +1,30 @@
 package notes;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Note00_Sample {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Sup Om!");
+		
+		
+		System.out.print("Enter an integer: ");
+		Scanner inkey = new Scanner(System.in);
+		
+		try {
+			int num = inkey.nextInt();
+		}
+		catch (InputMismatchException e) {
+			
+			System.out.println("Hey Dummy, that's not an integer");
+			
+		}
+		
+		
+		
+		
 		
 	}
 	
